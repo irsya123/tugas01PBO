5f6b748f0068241f019564893b5ab854c212d682
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-03-24 15:50:17 +0800

    Membuat atribut dan behavior

--- src/tugas01pbo/Sepatu.java
+++ src/tugas01pbo/Sepatu.java
@@ -10,5 +10,18 @@ package tugas01pbo;
  * @author ASUS
  */
 public class Sepatu {
+    String merk;
+    String bahan;
+    String ukuran;
+    String warna;
+    String jenis;
+    
+    void cetakinfo(){
+        System.out.println("Merk :"+merk);
+        System.out.println("Bahan :"+bahan);
+        System.out.println("Ukuran :"+ukuran);
+        System.out.println("Warna :"+warna);
+        System.out.println("Jenis :"+jenis);
+    }
     
 }
